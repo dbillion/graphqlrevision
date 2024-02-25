5073d56fb7e5b2ce9d9b766e58539ee1d4e0d235
dbillion <dayozoe@gmail~.com>
2024-02-25 13:05:47 +0200

    query unadulterated

---
+++ resolvers/Genre.js
@@ -0,0 +1,18 @@
+
+exports.Genre={
+           courses: (parent, args, context) => {
+            const courses = context.courses;
+ const genreId = parent.id;
+ const { filter } = args;
+ const genreProducts = courses.filter(item => item.
+genreId === genreId);
+ let filteredGenre = genreProducts;
+ if(filter){
+ if(filter.discount){
+ filteredGenre = filteredGenre.filter(product => 
+product.discount);
+ }
+ }
+ return filteredGenre;    }
+
+}
